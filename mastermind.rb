2e90f06a36ddf2b5471cf32331d4adba3ed64daa
Oliver <oc@hejverden.dk>
2017-09-29 12:23:12 +0200

    Add a method that evaluates the guess, and an introduction

--- mastermind.rb
+++ mastermind.rb
@@ -12,12 +12,58 @@ class Mastermind
 
   def initialize
     @code = []
+    @rounds_count = 0
+
+    generate_code
   end
   
   def generate_code
     4.times { @code << rand(1..6) }
+    puts "The code is: #{@code}"
   end
 
-  
+  def evaluate(guess)
+    answer = [" ", " ", " ", " "]
+    # Duplicate temp_code so it doesn't point directly to @code (destructive)
+    temp_code = @code.dup
+
+    # Correct color and position = ●
+    temp_code.each_with_index do |x, index|
+      if x == guess[index]
+        answer[index] = "●"
+        # Remove the used color (to avoid false duplicates)
+        temp_code[index] = nil
+      end
+    end
+
+    # Correct color but WRONG position = ○
+    guess.each_with_index do |x, index|
+      if temp_code.include?(x) && answer[index] != "●"
+        answer[index] = "○"
+        # Remove the used color by finding the index
+        temp_code[temp_code.index(x)] = nil
+      end
+    end
+
+    @rounds_count += 1
+    answer
+  end
+
+
 
 end
+
+puts "Welcome to Mastermind!"
+puts "Your job is to guess a combination of 4 numbers."
+puts "Each number is between 1-6."
+puts " "
+puts "You will get feedback after each guess:"
+puts " "
+puts "● = Correct number and position."
+puts "○ = The number exists in the combination but this is not the position."
+puts "Nothing equals a wrong number."
+puts " "
+
+
+
+
